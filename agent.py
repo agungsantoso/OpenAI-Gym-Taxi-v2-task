1c1f2185f33b8d091d3b77462d272af731aa2805
Agung Santoso <agung.santoso.pranoto@gmail.com>
2019-09-03 14:38:14 +0700

    First attempt

--- agent.py
+++ agent.py
@@ -12,6 +12,10 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        #init epsilon, alpha, gamma
+        self.eps = 0.005
+        self.alpha = 0.05
+        self.gamma = 1.0
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +28,14 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        # Define probability distrb. for eps-greedy policy for state
+        policy_state = np.ones(self.nA) * self.eps / self.nA
+        action_greedy = np.argmax(self.Q[state])
+        policy_state[action_greedy] = 1 - self.eps + (self.eps / self.nA)
+        
+        # Define action from eps-greedy policy
+        action = np.random.choice(self.nA, p=policy_state)        
+        return action
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +48,9 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        # Get the next action for the next state
+        next_action = self.select_action(next_state)
+        # Calculate discounted reward for next state & next action
+        G_t  = reward + self.gamma * self.Q[next_state][next_action]
+        # Update knowledge
+        self.Q[state][action] += self.alpha * (G_t - self.Q[state][action])
